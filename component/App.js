6d9c3eea5016e8ae85beb970398901454d607f82
อธิษฐาน ศรีวิเชียร <s6002041510071@Macmini-213-07.local>
2019-03-14 16:38:41 +0700

    create App component

---
+++ component/App.js
@@ -0,0 +1,17 @@
+// import library
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// write component
+class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+}
+
+//export
+export default App;
